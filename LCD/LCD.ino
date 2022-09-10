e29b66d0ff95dbe7f1e5776121b9183804bdca98
caroliny silva <caroliny.silv4@outlook.com>
2022-09-10 15:40:43 -0300

    Update LCD.ino

--- LCD/LCD.ino
+++ LCD/LCD.ino
@@ -4,18 +4,28 @@
 #include <LiquidCrystal.h>
 
 LiquidCrystal lcd(2,4,10,11,12,13); 
-
+long duration;
+int distance;
+const int trig = 8;
+const int echo = 9;
 void setup() {
-  
   lcd.begin(16,2); //informar o número de colunas e linhas
-  lcd.print("Ola Mundo!"); // mostra a mensagem na tela
-  
-  lcd.setCursor(0,1);//mostra a próxima mensagem na parte inferior 
-  lcd.write('LCD 16x4');
-
-} 
-
-void loop() {
-  
-
+  lcd.print("W.A.R 8066"); // mostra a mensagem na tela
+  pinMode(trig, OUTPUT); // Inicia o trigPin como saida
+  pinMode(echo, INPUT); // Inicia echoPin como entrada
+  Serial.begin(9600); // Inicia a comunicação
 }
+void loop() {
+  distance = calculateDistance();
+  Serial.println(distance);
+}
+  int calculateDistance(){
+  digitalWrite(trig, LOW);
+  delayMicroseconds(5);
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(50);
+  digitalWrite(trig, LOW);
+  duration = pulseIn(echo, HIGH);
+  distance= duration*0.034/2;
+  return distance; // retorna a distancia 
+} 
